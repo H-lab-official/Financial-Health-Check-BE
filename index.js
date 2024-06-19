82a5493e78fb17e5720cc981122cfe16c6b1f0e1
enterpannet <enterpannet@gmail.com>
2024-06-19 18:28:14 +0700

    back to start

--- index.js
+++ index.js
@@ -18,10 +18,26 @@ import { swaggerOptions } from './swaggerOptions.js';
 const app = express();
 const port = process.env.MAIN_PORT || 3000;
 
+// Use Helmet with custom CSP settings
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:"],
+        connectSrc: ["'self'"],
+        fontSrc: ["'self'"],
+        objectSrc: ["'none'"],
+        mediaSrc: ["'self'"],
+        frameSrc: ["'none'"],
+      },
+    },
+  })
+);
 
-app.use(helmet());
-
-
+// Configure CORS with allowed origin and credentials
 const allowedOrigins = ['https://financial-health-check.azayagencyjourney.com',"http://localhost:5173","http://192.168.1.37:5173"];
 app.use(
   cors({
