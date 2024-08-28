cc20d8f7d9f5baf1b393215b9c46a53e58fd02e5
enterpannet <enterpannet@gmail.com>
2024-08-28 14:35:15 +0700

    edit cord

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(
 );
 
 // Configure CORS with allowed origin and credentials
-const allowedOrigins = ['https://financial-health-check.azayagencyjourney.com', "http://localhost:5173", "http://192.168.1.33:5173"];
+const allowedOrigins = ['https://financial-health-check.azayagencyjourney.com', "http://localhost:5173", "http://192.168.1.33:5173", "http://localhost:4173", "http://localhost:3173"];
 app.use(
   cors({
     origin: function (origin, callback) {
